b3ab936316bc97cc9351659cabfbd4bcfc165a92
Maria Khan <mariapervaiz40@gmail.com>
2024-04-04 07:14:40 +0500

    Added more functionalities(Add,Delete,Update,Exit)

--- index.js
+++ index.js
@@ -1,36 +1,76 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-let todos = [];
+import chalk from "chalk";
 let condition = true;
-while (condition) {
-    const answers = await inquirer.prompt([{
-            name: "TODO",
-            type: "input",
-            message: "What do you want to add in your todo?"
-        },
-        {
-            name: "addmore",
-            type: "confirm",
-            message: "Do you want to add more todo?",
-            default: false
+let todos = [];
+async function makeTodo(todos) {
+    while (condition) {
+        let ans = await inquirer.prompt([
+            {
+                name: "operation",
+                type: "list",
+                message: chalk.bold.red("Select any one operation.."),
+                choices: ["Add", "Update", "View", "Delete", "Exit"],
+            },
+        ]);
+        if (ans.operation === "Add") {
+            let addTodo = await inquirer.prompt([
+                {
+                    name: "todo",
+                    type: "input",
+                    message: chalk.bgMagenta.red("Add items in your List.."),
+                },
+                {
+                    name: "addmore",
+                    type: "confirm",
+                    message: "Do you want to add more todo?",
+                    default: false,
+                },
+            ]);
+            todos.push(addTodo.todo);
+            console.log(chalk.greenBright(todos));
         }
-    ]);
-    const { TODO, addmore } = answers;
-    console.log(answers);
-    condition = addmore;
-    if (TODO) {
-        todos.push(TODO);
+        if (ans.operation === "Update") {
+            let updateTodo = await inquirer.prompt([
+                {
+                    name: "todo",
+                    type: "list",
+                    message: chalk.bgYellow.red("Choose an Item you want to update in your List"),
+                    choices: todos.map((item) => item),
+                },
+            ]);
+            let addTodo = await inquirer.prompt([
+                {
+                    name: "todo",
+                    type: "input",
+                    message: "Enter updated item..",
+                },
+            ]);
+            let updatedtodo = todos.filter((value) => value !== updateTodo.todo);
+            todos = [...updatedtodo, addTodo.todo];
+            console.log(todos);
+        }
+        if (ans.operation === "View") {
+            console.log(chalk.bold.whiteBright("Your To-Do List"));
+            console.log(todos);
+        }
+        if (ans.operation === "Delete") {
+            let deleteTodo = await inquirer.prompt([
+                {
+                    name: "todo",
+                    type: "list",
+                    message: chalk.redBright.red("Delete items in your List"),
+                    choices: todos.map((item) => item),
+                },
+            ]);
+            let updatedtodo = todos.filter((value) => value !== deleteTodo.todo);
+            todos = [...updatedtodo];
+            console.log(todos);
+        }
+        if (ans.operation === "Exit") {
+            condition = false;
+            console.log(chalk.bgYellowBright.bold.red("---- Thanks for using ToDo App..! ----"));
+        }
     }
-    else {
-        console.log("Kindly add valid input");
-    }
 }
-if (todos.length > 0) {
-    console.log("Your Todo List:");
-    todos.forEach(todo => {
-        console.log(todo);
-    });
-}
-else {
-    console.log("No todos found");
-}
+makeTodo(todos);
